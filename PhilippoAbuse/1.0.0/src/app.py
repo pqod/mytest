d7b0bdb95c0ed09a9d716184c7a9c60ec360e8bd
sebastien <francesco.sobrio@gmail.com>
2022-10-11 22:37:16 +0200

    testing

---
+++ PhilippoAbuse/1.0.0/src/app.py
@@ -0,0 +1,32 @@
+import asyncio
+import urllib.request
+from bs4 import BeautifulSoup
+from walkoff_app_sdk.app_base import AppBase
+
+
+class GetAbuse(AppBase):
+    """
+    An example of a Walkoff App.
+    Inherit from the AppBase class to have Redis, logging, and console logging set up behind the scenes.
+    """
+    __version__ = "1.0.0"
+    app_name = "PhilippoAbuse"  # this needs to match "name" in api.yaml
+
+    def __init__(self, redis, logger):
+        """
+        Each app should have this __init__ to set up Redis and logging.
+        :param redis:
+        :param logger:
+        """
+        super().__init__(redis, logger)
+    async def get_ip(self,url):
+        req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        rawsite = urllib.request.urlopen(req)
+        content = rawsite.read().decode('utf-8')
+        soup = BeautifulSoup(content, 'html.parser')
+        res = soup.find(class_="col-md-4").find_all("a")
+        filtered = map(lambda ip: ip.contents[0], res)
+        return(list(filtered))
+
+if __name__ == "__main__":
+    asyncio.run(Basics.run())
